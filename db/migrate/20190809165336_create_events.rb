0e91d86cc17b59face4dc67ece8b0fc8939a9b86
jollyjerr <jollyjerr@gmail.com>
2019-08-09 11:10:29 -0600

    update schema

--- db/migrate/20190809165336_create_events.rb
+++ db/migrate/20190809165336_create_events.rb
@@ -1,6 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
+      t.string :name
+      t.string :time
+      t.text :description
+      t.string :speaker_name
+      t.string :speaker_url
+      t.integer :location_id
 
       t.timestamps
     end
